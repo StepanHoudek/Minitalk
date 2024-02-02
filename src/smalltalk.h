4c134dd29b05f4cf6365fba5ecf65d528c3c051f
shoudek <stepan.houdek1@gmail.com>
2024-02-02 13:33:54 +0100

    Update: add signal.h

--- src/smalltalk.h
+++ src/smalltalk.h
@@ -6,7 +6,7 @@
 /*   By: shoudek <shoudek@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/02 13:07:46 by shoudek           #+#    #+#             */
-/*   Updated: 2024/02/02 13:08:31 by shoudek          ###   ########.fr       */
+/*   Updated: 2024/02/02 13:10:55 by shoudek          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,6 @@
 # define SMALLTALK_H
 
 #include "../libft/libft.h"
+#include <signal.h>
 
 #endif

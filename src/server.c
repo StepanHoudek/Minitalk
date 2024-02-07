d0c3e25d6a1523a095fb75de80f4fae09eeccc8d
shoudek <stepan.houdek1@gmail.com>
2024-02-07 15:26:59 +0100

    Clean: clean unused code

--- src/server.c
+++ src/server.c
@@ -6,48 +6,35 @@
 /*   By: shoudek <shoudek@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/02 12:40:09 by shoudek           #+#    #+#             */
-/*   Updated: 2024/02/07 14:38:32 by shoudek          ###   ########.fr       */
+/*   Updated: 2024/02/07 15:24:10 by shoudek          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// client receives string and server pid
-// client transcodes string to binary and sends individual 1 and 0 to server
-// server initiates a sigset and adds the signals to the set
-// client ends the message with sending '/0' encoded in binary OR
-// 		server doesnt receive a signal for X miliseconds
-// server then decodes from binary to ascii and prints out
-
-// First step - send multiple signals to server
-
 #include "minitalk.h"
-
-unsigned char	c;
 
 void	signal_handler(int signum)
 {
 	static int				bit_itr = -1;
 	static unsigned char	c = 0;
 
-	printf("Received signal |%d|\n", signum);
 	if (bit_itr < 0)
 		bit_itr = 8;
-	c <<= 1;
+	c <<= 0b00000001;
 	if (signum == 12)
 		c |= 1;
 	bit_itr--;
 	if (bit_itr == 0)
 	{
-		ft_printf("Character: |%c|\n", c);
+		ft_printf("%c", c);
 		bit_itr = -1;
 		c = 0;
 	}
 	return ;
-	// print_array();
 }
 
-int	main(void)
+void	config_signals(void)
 {
-	struct sigaction action;
+	struct sigaction	action;
 
 	action.sa_handler = signal_handler;
 	sigemptyset(&action.sa_mask);
@@ -56,23 +43,12 @@ int	main(void)
 	action.sa_flags = 0;
 	sigaction(SIGUSR1, &action, NULL);
 	sigaction(SIGUSR2, &action, NULL);
+}
 
-	ft_printf("%d", getpid());
-
+int	main(void)
+{
+	ft_printf("Server pid: %d\n", getpid());
 	while (1)
-		;
-	return (0);
-
-	// signal_handler(SIGUSR2);
-	// signal_handler(SIGUSR1);
-	// signal_handler(SIGUSR2);
-	// signal_handler(SIGUSR2);
-	// signal_handler(SIGUSR1);
-	// signal_handler(SIGUSR2);
-	// signal_handler(SIGUSR1);
-
-	// int i = 7;
-	// while (i--)
-	// 	signal_handler(SIGUSR1);
-	// return (0);
+		config_signals();
+	return (EXIT_SUCCESS);
 }
